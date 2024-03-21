3ecc2a1dc6cbe70966906533a52913dc7620cf89
Matilda <ogarimatilda@gmail.com>
2024-03-21 13:07:36 +0300

    assignment completed

---
+++ index.js
@@ -0,0 +1,20 @@
+function divide() {
+    let a = 2000
+    let b = 100
+    console.log(`dividing ${a}`)
+    console.log(`dividing ${b}`)
+    return a / b
+}
+
+const square = (x) => {
+    return x * x;
+}
+  
+const add =() => {
+    let a = 3
+    let b = 4
+    console.log(`adding $(a)`)
+    console.log(`adding $(b)`)
+    return a + b
+}
+  
